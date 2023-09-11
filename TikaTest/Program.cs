95acae202942af58c138d8043dd8a490cc5d6a2e
Matt Bayliss <matt.bayliss@icognition.com.au>
2023-09-11 16:51:20 +1000

    Initial commit of work so far

---
+++ TikaTest/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using org.apache.tika;
+using org.apache.tika.sax;
+using org.apache.tika.parser;
+using ikvm.io;
+using org.apache.tika.metadata;
+using org.apache.tika.detect;
+using org.apache.tika.parser.microsoft.ooxml;
+using org.apache.tika.parser.microsoft;
+using org.apache.tika.config;
+
+var filePath = @"C:\Temp\test.docx";
+
+using var fs = new FileStream(filePath, FileMode.Open);
+using var stream = new InputStreamWrapper(fs);
+
+Parser parser1 = new OOXMLParser();
+Parser parser2 = new OfficeParser();
+
+BodyContentHandler handler = new BodyContentHandler();
+Parser parser = new AutoDetectParser();
+Metadata metadata = new Metadata();
+ParseContext context = new ParseContext();
+
+parser.parse(stream, handler, metadata, context);
+
+string text = handler.toString();
+
+using (var sw = new StreamWriter(@"C:\Temp\output.txt", false))
+{
+    sw.WriteLine(text);
+}
+Console.WriteLine($"{filePath}: {text}");
